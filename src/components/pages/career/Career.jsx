47a08cdd6c85cd01e9d75fafd058b4904175441f
Giorgi <georgeeantia@gmail.com>
2022-11-15 15:41:27 +0400

    CareerDesktop-ModalForm

--- src/components/pages/career/Career.jsx
+++ src/components/pages/career/Career.jsx
@@ -7,10 +7,12 @@ import testImage from "../../../img/aboutUsCard2.png";
 import {ReactComponent as CardArrowLeft} from "../../../img/pageArrowLeft.svg";
 import {ReactComponent as CardArrowRight} from "../../../img/pageArrowRight.svg";
 import {Link} from "react-router-dom";
+import CareerModal from "./CareerModal";
 
 function Career() {
     const [activeCard, setActiveCard] = useState(1);
     const ref = useRef(null);
+    const [showCareerModal, setShowCareerModal] = useState(false);
 
     const handleClick = (id) => {
         setActiveCard(id);
@@ -43,126 +45,166 @@ function Career() {
     }
 
     return (
-        <div className="pages-header">
+        <>
+            <div className="pages-header">
 
-            <div className="career-top">
-                <div className="contact-gradient-top"></div>
-                <div className="contact-gradient-bottom"></div>
-            </div>
-
-            <div className="career-content">
-                <div className="career-left-side">
-                    <h1 className="career-top-text">კარიერა</h1>
-                    <div className='career-top-header'><h1>#გახდიუნისონელი</h1></div>
-                    <p>თუ გსურს შემოუერთდე ჩვენ გუნდს, დაგვიტოვე ინფორმაცია:</p>
-                    <Button className="upload-cv btn-primary" variant="primary" type="submit">
-                        <Upload/>
-                        ატვირთეთ CV
-                    </Button>
-                    <p> ან <span className="career-form-image"><FormImage/></span>
-                        <span className="career-form-open">შეავსეთ ფორმა</span>
-                    </p>
+                <div className="career-top">
+                    <div className="contact-gradient-top"></div>
+                    <div className="contact-gradient-bottom"></div>
                 </div>
 
-                <div className="career-right-side">
-                    <h1>ჩვენი HR გუნდი</h1>
-                    <p>
-                        “უნისონი” ერთ-ერთი წამყვანი სადაზღვევო კომპანიაა საქართველოში. ამაში კი, უდიდესი წვლილი მისი
-                        ლოიალური თანამშრომლების გუნდს მიუძღვის, რომელიც კომპანიას 16 წლის მანძილზე ზრდის და ავითარებს.
-                        სწორედ გუნდური და ჯანსაღი გარემოა ის უმნიშვნელოვანესი პირობა, რომლის მეშვეობით, კომპანიამ
-                        მოახერხა ბაზარზე საიმედო დამსაქმებლის სახელით პოზიციონირება და გახდა სასურველი, სანდო სადაზღვევო
-                        კომპანია თავისი კლიენტებისთვის.
-                    </p>
-                    <p>
-                        ადამიანური რესურსების მართვის სტრატეგია ორგანიზაციის ბიზნეს სტრატეგიას ეყრდნობა და
-                        ორიენტირებულია გუნდის ეფექტურ მართვაზე, რომელიც ხელს უწყობს კომპანიას ზრდაში,
-                        კონკურენტუნარიანობასა და სასურველი შედეგების მიღწევაში.
-                    </p>
-                    <p>
-                        ჩვენი ადამიანური რესურსების მართვის მიმართულებისთვის პრიორიტეტულია კომფორტული სამუშაო გარემოს
-                        შექმნა, თანამშრომლებს შორის მოტივაციის ამაღლება, მათი უნარებისა და პოტენციალის მაქსიმალურად
-                        გამოვლინების ხელშეწყობა და ზრუნვა მათ კმაყოფილებასა და განვითარებაზე. ასევე, არანაკლები
-                        ყურადღება ეთმობა კომპანიის ზრდის ფონზე, თანამშრომელთა ლოიალობის შენარჩუნებას.
-                    </p>
-                    <p>
-                        ჩვენი გუნდის გამორჩეული თვისებაა კოლეგიურობა და ძალიან პოზიტიური ურთიერთთანამშრომლობა.
-                        დაკოპირებულია ირაოდან
-                    </p>
-
-                </div>
-            </div>
-
-            <div className="career-team">
-                <div className="career-team-card-header">
-                    <span>მენეჯმენტის გუნდი</span>
+                {showCareerModal ? (<CareerModal show={showCareerModal} close={() => setShowCareerModal(false)}/>) : (
                     <div>
-                        <span onClick={handleIdDecrease}><CardArrowLeft/></span>
-                        <span onClick={handleIdIncrease}><CardArrowRight/></span>
-                    </div>
-                </div>
-                <div className="career-team-cards" ref={ref}>
-
-                    <div className={`career-team-card ${activeCard === 1 && 'active'}`} onClick={() => handleClick(1)}>
-                        <div className="career-team-image">
-                            <img src={testImage} alt="testImage"/>
-                        </div>
-                        <div className="career-team-text-container">
-                            <div className="career-team-text-title">
-                                <h3>გიორგი გიორგაძე</h3>
-                                <span>გენერალური დირექტორი</span>
+                        <div className="career-content">
+                            <div className="career-left-side">
+                                <h1 className="career-top-text">კარიერა</h1>
+                                <div className='career-top-header'><h1>#გახდიუნისონელი</h1></div>
+                                <p>თუ გსურს შემოუერთდე ჩვენ გუნდს, დაგვიტოვე ინფორმაცია:</p>
+                                <Button className="upload-cv btn-primary" variant="primary" type="submit">
+                                    <label htmlFor="upload-cv" className="career-modal-add-btn">
+                                        <Upload/>
+                                        ატვირთეთ CV
+                                    </label>
+                                    <input id="upload-cv" className="upload-cv-input" type="file"
+                                           name="upload-cv"/>
+                                </Button>
+                                <p> ან <span className="career-form-image"><FormImage/></span>
+                                    <span className="career-form-open" onClick={() => setShowCareerModal(true)}>
+                                შეავსეთ ფორმა
+                            </span>
+                                </p>
                             </div>
-                            <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
-                                უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის მაგისტრის
-                                ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004 წლიდან მუშაობს
-                                სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p></div>
 
-                        </div>
-                    </div>
+                            <div className="career-right-side">
+                                <h1>ჩვენი HR გუნდი</h1>
+                                <p>
+                                    “უნისონი” ერთ-ერთი წამყვანი სადაზღვევო კომპანიაა საქართველოში. ამაში კი, უდიდესი
+                                    წვლილი მისი
+                                    ლოიალური თანამშრომლების გუნდს მიუძღვის, რომელიც კომპანიას 16 წლის მანძილზე ზრდის და
+                                    ავითარებს.
+                                    სწორედ გუნდური და ჯანსაღი გარემოა ის უმნიშვნელოვანესი პირობა, რომლის მეშვეობით,
+                                    კომპანიამ
+                                    მოახერხა ბაზარზე საიმედო დამსაქმებლის სახელით პოზიციონირება და გახდა სასურველი,
+                                    სანდო
+                                    სადაზღვევო
+                                    კომპანია თავისი კლიენტებისთვის.
+                                </p>
+                                <p>
+                                    ადამიანური რესურსების მართვის სტრატეგია ორგანიზაციის ბიზნეს სტრატეგიას ეყრდნობა და
+                                    ორიენტირებულია გუნდის ეფექტურ მართვაზე, რომელიც ხელს უწყობს კომპანიას ზრდაში,
+                                    კონკურენტუნარიანობასა და სასურველი შედეგების მიღწევაში.
+                                </p>
+                                <p>
+                                    ჩვენი ადამიანური რესურსების მართვის მიმართულებისთვის პრიორიტეტულია კომფორტული
+                                    სამუშაო
+                                    გარემოს
+                                    შექმნა, თანამშრომლებს შორის მოტივაციის ამაღლება, მათი უნარებისა და პოტენციალის
+                                    მაქსიმალურად
+                                    გამოვლინების ხელშეწყობა და ზრუნვა მათ კმაყოფილებასა და განვითარებაზე. ასევე,
+                                    არანაკლები
+                                    ყურადღება ეთმობა კომპანიის ზრდის ფონზე, თანამშრომელთა ლოიალობის შენარჩუნებას.
+                                </p>
+                                <p>
+                                    ჩვენი გუნდის გამორჩეული თვისებაა კოლეგიურობა და ძალიან პოზიტიური
+                                    ურთიერთთანამშრომლობა.
+                                    დაკოპირებულია ირაოდან
+                                </p>
 
-                    <div className={`career-team-card ${activeCard === 2 && 'active'}`} onClick={() => handleClick(2)}>
-                        <div className="career-team-image">
-                            <img src={testImage} alt="testImage"/>
-                        </div>
-                        <div className="career-team-text-container">
-                            <div className="career-team-text-title">
-                                <h3>გიორგი გიორგაძე</h3>
-                                <span>გენერალური დირექტორი</span>
                             </div>
-                            <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
-                                უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის მაგისტრის
-                                ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004 წლიდან მუშაობს
-                                სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p></div>
-
                         </div>
-                    </div>
 
-                    <div className={`career-team-card ${activeCard === 3 && 'active'}`} onClick={() => handleClick(3)}>
-                        <div className="career-team-image">
-                            <img src={testImage} alt="testImage"/>
-                        </div>
-                        <div className="career-team-text-container">
-                            <div className="career-team-text-title">
-                                <h3>გიორგი გიორგაძე</h3>
-                                <span>გენერალური დირექტორი</span>
+
+                        <div className="career-team">
+                            <div className="career-team-card-header">
+                                <span>მენეჯმენტის გუნდი</span>
+                                <div>
+                                    <span onClick={handleIdDecrease}><CardArrowLeft/></span>
+                                    <span onClick={handleIdIncrease}><CardArrowRight/></span>
+                                </div>
                             </div>
-                            <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
-                                უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის მაგისტრის
-                                ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004 წლიდან მუშაობს
-                                სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p></div>
+                            <div className="career-team-cards" ref={ref}>
+
+                                <div className={`career-team-card ${activeCard === 1 && 'active'}`}
+                                     onClick={() => handleClick(1)}>
+                                    <div className="career-team-image">
+                                        <img src={testImage} alt="testImage"/>
+                                    </div>
+                                    <div className="career-team-text-container">
+                                        <div className="career-team-text-title">
+                                            <h3>გიორგი გიორგაძე</h3>
+                                            <span>გენერალური დირექტორი</span>
+                                        </div>
+                                        <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
+                                            უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის
+                                            მაგისტრის
+                                            ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004
+                                            წლიდან
+                                            მუშაობს
+                                            სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className={`career-team-card ${activeCard === 2 && 'active'}`}
+                                     onClick={() => handleClick(2)}>
+                                    <div className="career-team-image">
+                                        <img src={testImage} alt="testImage"/>
+                                    </div>
+                                    <div className="career-team-text-container">
+                                        <div className="career-team-text-title">
+                                            <h3>გიორგი გიორგაძე</h3>
+                                            <span>გენერალური დირექტორი</span>
+                                        </div>
+                                        <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
+                                            უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის
+                                            მაგისტრის
+                                            ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004
+                                            წლიდან
+                                            მუშაობს
+                                            სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className={`career-team-card ${activeCard === 3 && 'active'}`}
+                                     onClick={() => handleClick(3)}>
+                                    <div className="career-team-image">
+                                        <img src={testImage} alt="testImage"/>
+                                    </div>
+                                    <div className="career-team-text-container">
+                                        <div className="career-team-text-title">
+                                            <h3>გიორგი გიორგაძე</h3>
+                                            <span>გენერალური დირექტორი</span>
+                                        </div>
+                                        <div className="career-team-text"><p>2006 წელს დაამთავრა თბილისის სახელმწიფო
+                                            უნივერსიტეტი-საერთაშორისო სამართლის ფაკულტეტი. ფლობს საერთაშორისო სამართლის
+                                            მაგისტრის
+                                            ხარისხს. აქვს სადაზღვევო სექტორში მუშაობის 15 წლიანი გამოცდილება. 2004
+                                            წლიდან
+                                            მუშაობს
+                                            სადაზღვევო კომპანია ალდაგში, სადაც მისი კარიერა კორპორატიული კლიენტების…</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div className="career-team-card-footer">
+                                <h5>ინფორმაცია კომპანიის ბენეფიციარების და უშუალო მესაკუთრის შესახებ</h5>
+                                <p>ინფორმაცია მზღვეველის აქციების 10 პროცენტის ან 10 პროცენტზე მეტის მფლობელი უშუალო
+                                    მესაკუთრის
+                                    შესახებ ნახვა შეგიძლიათ <Link to="#">ბმულზე.</Link></p>
+                                <p> ინფორმაცია მზღვეველის მნიშვნელოვანი წილის მფლობელი ბენეფიციარი მესაკუთრის შესახებ
+                                    ნახვა
+                                    შეგიძლიათ <Link to="#">ბმულზე.</Link></p>
+                            </div>
                         </div>
                     </div>
-
-
-                </div>
-                <div className="career-team-card-footer">
-                    <h5>ინფორმაცია კომპანიის ბენეფიციარების და უშუალო მესაკუთრის შესახებ</h5>
-                    <p>ინფორმაცია მზღვეველის აქციების 10 პროცენტის ან 10 პროცენტზე მეტის მფლობელი უშუალო მესაკუთრის
-                        შესახებ ნახვა შეგიძლიათ <Link to="#">ბმულზე.</Link></p>
-                    <p> ინფორმაცია მზღვეველის მნიშვნელოვანი წილის მფლობელი ბენეფიციარი მესაკუთრის შესახებ ნახვა
-                        შეგიძლიათ <Link to="#">ბმულზე.</Link></p>
-                </div>
+                )}
             </div>
-        </div>
+        </>
     );
 }
 
---
+++ src/components/pages/career/CareerModal.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import '../../../styles/career.scss';
+import {Button, Form} from "react-bootstrap";
+import {ReactComponent as CareerModalClose} from "../../../img/partnerClose.svg";
+import {ReactComponent as Add} from "../../../img/add.svg";
+import {ReactComponent as Send} from "../../../img/send.svg"
+
+
+function CareerModal(props) {
+
+
+    return (
+
+        <div className="career-modal">
+            <div className="career-modal-header">
+                <span>შეავსეთ ფორმა</span>
+                <CareerModalClose onClick={props.close}/>
+            </div>
+            <div className="career-modal-body">
+                <Form>
+
+                    <div className="career-modal-form">
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="name">სახელი</label>
+                                <input className="form-control" type="text" placeholder="სახელი" id="name"
+                                       name="name"/>
+                            </div>
+                        </div>
+
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="last-name">გვარი</label>
+                                <input className="form-control" type="text" placeholder="გვარი" id="last-name"
+                                       name="last-name"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="phone-number">ტელეფონი</label>
+                                <input className="form-control" type="tel" placeholder="ტელეფონი" id="phone-number"
+                                       name="phone"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="email">ელ.ფოსტა</label>
+                                <input className="form-control" type="email" placeholder="ელ.ფოსტა" id="email"
+                                       name="email"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="birthdate">დაბადების თარიღი</label>
+                                <input className="form-control" type="date" placeholder="დაბადების თარიღი"
+                                       id="birthdate"
+                                       name="birthdate"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="city">რომელ ქალაქში გსურთ მუშაობა?</label>
+                                <input className="form-control" type="text" placeholder="ქალაქი" id="city"
+                                       name="city"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="position">რა დონის პოზიცია გაინტერესებთ?</label>
+                                <input className="form-control" type="text" placeholder="პოზიცია" id="position"
+                                       name="position"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="job">რა მიმართულებით გსურთ მუშაობა?</label>
+                                <input className="form-control" type="text" placeholder="პოზიცია" id="job"
+                                       name="job"/>
+                            </div>
+                        </div>
+                        <div className="career-modal-input">
+                            <div className="career-form-group">
+                                <label htmlFor="salary">სასურველი ანაზღაურება</label>
+                                <input className="form-control" type="text" placeholder="სასურველი ანაზღაურება"
+                                       id="salary"
+                                       name="salary"/>
+                                <div className="career-modal-gel"><span>₾</span></div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="modal-career-footer">
+
+                        <span className="career-modal-add-title">განათლება</span>
+                        <label htmlFor="education" className="career-modal-add-btn">
+                            <Add/> განათლების დამატება
+                        </label>
+                        <input id="education" className="career-modal-footer-input" type="file"
+                               name="education"/>
+
+
+                        <span className="career-modal-add-title">სამუშაო გამოცდილება</span>
+                        <label htmlFor="work-experience" className="career-modal-add-btn">
+                            <Add/> სამუშაო გამოცდილების დამატება
+                        </label>
+                        <input id="work-experience" className="career-modal-footer-input" type="file"
+                               name="work-experience"/>
+
+                        <div className="career-modal-add-title"><p>მსურს ელ-ფოსტაზე მივიღო კომპანიის მიმდინარე
+                            ვაკანსიები და სიახლეები</p>
+
+                            <div className="d-flex my-3">
+                                <div className="form-check ">
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                           id="flexRadioDefault1" checked/>
+                                    <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                        დიახ
+                                    </label>
+                                </div>
+                                <div className="form-check mx-3">
+                                    <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                           id="flexRadioDefault2"/>
+                                    <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                        არა
+                                    </label>
+                                </div>
+                            </div>
+                            <Button className="career-modal-btn btn-primary" variant="primary" type="submit">
+                                <Send/>გაგზავნა
+                            </Button>
+                        </div>
+
+
+                    </div>
+                </Form>
+            </div>
+        </div>
+    );
+}
+
+export default CareerModal;
